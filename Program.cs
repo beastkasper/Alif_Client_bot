f38b9e2b9518d1281183bac71895cb5782b272fe
unknown <aminmirzoev123@gmail.com>
2020-04-21 18:21:12 +0500

    sign in

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using BD_SQL;
 
 namespace Console_Bank_Alif_C_
 {
+    
     class Program
     {
+          const string Const = @"Data Source=localhost; Initial Catalog=master;Integrated Security=True";
+
         static void Main(string[] args)
         {
+            SqlConnection conn =new SqlConnection();
             bool work=true;
                 while(work)
                 {
@@ -16,8 +23,28 @@ namespace Console_Bank_Alif_C_
                     System.Console.WriteLine("5->'Оформление кредита с графиком погашения'");
                     System.Console.WriteLine("6->'Выход'");
 
-                        
                 }
+                     
+                        
+        }
+
+        static void Sign_in(SqlConnection conn)
+        {
+            Console.Clear();
+            System.Console.WriteLine("Введите свое имя:");
+            string Name= Console.ReadLine();
+            System.Console.WriteLine("Введите свою Фамилию:");
+            string Surname=Console.ReadLine();
+            System.Console.WriteLine("Введите своe Отчество(не обязательно):");
+            string MiddleName=Console.ReadLine();
+            System.Console.WriteLine("Введите свой адрес:");
+            string address=Console.ReadLine();
+            System.Console.WriteLine("Введите свой город проживания:");
+            string City=Console.ReadLine();
+            System.Console.WriteLine("Введите свою почту:");
+            string gmail=Console.ReadLine();
+            System.Console.WriteLine("Введите свой пол:");
+            string sex=Console.ReadLine();
         }
     }
 }
--- SQLBD.cs
+++ SQLBD.cs
@@ -1,7 +1,16 @@
 namespace BD_SQL
+
 {
+
     public class SQLBD
     {
+        static void BD()
+        {
             const string Const = @"Data Source=localhost; Initial Catalog=master;Integrated Security=True";
+
+        }
+          
+
     }
+
 }
