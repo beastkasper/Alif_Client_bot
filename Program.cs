61c7078fc6c61b99282fbef389502e7f083604c3
unknown <aminmirzoev123@gmail.com>
2020-04-21 15:57:48 +0500

    starter

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Console_Bank_Alif_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool work=true;
+                while(work)
+                {
+                    System.Console.WriteLine("1->'Sign in'");
+                    System.Console.WriteLine("2->'Заполнение анкеты клиента'");
+                    System.Console.WriteLine("3->'Добавление заявки на кредит клиенту'");
+                    System.Console.WriteLine("4->'можно ли взять кредит или нет'");
+                    System.Console.WriteLine("5->'Оформление кредита с графиком погашения'");
+                    System.Console.WriteLine("6->'Выход'");
+
+                        
+                }
+        }
+    }
+}
---
+++ SQLBD.cs
@@ -0,0 +1,7 @@
+namespace BD_SQL
+{
+    public class SQLBD
+    {
+            const string Const = @"Data Source=localhost; Initial Catalog=master;Integrated Security=True";
+    }
+}
