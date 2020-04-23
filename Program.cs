e9857be4cac70a0208ad200be5b98abd0b0f99b4
unknown <aminmirzoev123@gmail.com>
2020-04-23 21:28:31 +0500

    заполнение анкеты

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace Console_Bank_Alif_C_
 
     class Program
     {
-        const string Const = @"Data Source=localhost; Initial Catalog=Account_user ;Integrated Security=True";
 
-
+        const string Const = @"Data Source=localhost; Initial Catalog=CADB ;Integrated Security=True";
+        static int id = 0;
         static void Main()
         {
 
@@ -43,6 +43,17 @@ namespace Console_Bank_Alif_C_
                     {
                         Regestretions(3);
                     }
+                    if (n == 2)
+                    {
+                        if (id != 0)
+                        {
+                            ComplateTheForm();
+                        }
+                        else
+                        {
+
+                        }
+                    }
                     if (n == 6)
                     {
                         Console.Clear();
@@ -65,7 +76,7 @@ namespace Console_Bank_Alif_C_
             string Name = Console.ReadLine();
             System.Console.Write("Введите свою Фамилию:");
             string Surname = Console.ReadLine();
-            System.Console.WriteLine("Введите свое Отчество:");
+            System.Console.Write("Введите свое Отчество:");
             string MiddleName = Console.ReadLine();
             System.Console.Write("Введите свой Address:");
             string Address = Console.ReadLine();
@@ -76,29 +87,125 @@ namespace Console_Bank_Alif_C_
             else if (sex == 'W') { System.Console.WriteLine("ок"); }
 
             else { System.Console.WriteLine("Error!"); goto E; }
-            Again:
+        Again:
             System.Console.Write("Введите свой номер телефона вместо логина:");
-             int Tel=int.Parse(Console.ReadLine());
-                if(Tel>0){System.Console.Write("fxgfc");}
-            else
+            int Tel = int.Parse(Console.ReadLine());
+
+            if (Tel < 0)
             {
                 System.Console.WriteLine("вы ввели не верно свои номра или ввели больше или меньше 9 цифр!!!");
 
                 goto Again;
             }
             System.Console.Write("Create Password (Mi-8):");
-            string pass=Console.ReadLine();
+            string Password = Console.ReadLine();
+            System.Console.WriteLine("введите Id паспорта");
+            string ICard=Console.ReadLine();
             SqlConnection conn = new SqlConnection(Const);
             conn.Open();
             SqlCommand command = new SqlCommand(Const, conn);
-            command.CommandText = $"INSERT INTO Sign_up ([Name],[Surname],[MiddleName],[Address],[sex],[phone_number],[Password],[RoleId]) VALUES ('{Name}','{Surname}','{MiddleName}','{Address}','{sex}',{Tel},'{pass}',{rid})";
+            command.CommandText = $"INSERT INTO U_Accaunt ([Name],[Surname],[MiddleName],[Address],[Gender],[Login],[RoleId],[Password],[ICard]) VALUES ('{Name}','{Surname}','{MiddleName}','{Address}','{sex}',{Tel},{rid},'{Password}',{ICard})";
             command.ExecuteNonQuery();
+
             System.Console.WriteLine("Вы успешно зарегистрировались!");
         }
 
         static void ComplateTheForm()
         {
-            
+            Console.Clear();
+            SqlConnection conn =new SqlConnection(Const);
+            conn.Open();
+            SqlCommand comm =new SqlCommand(Const,conn);
+
+            int count = 0;
+            System.Console.WriteLine("Для того что бы получить кредит заполните анкету!");
+
+            System.Console.WriteLine("Sex");
+            System.Console.Write("Male-1:");
+           System.Console.WriteLine("Female-2");
+            int sex  = int.Parse(Console.ReadLine());
+            if(sex==1)
+            {
+                count++;
+            }
+           if(sex==2)
+           {
+               count+=2;
+           }
+            System.Console.WriteLine("Семейное положение:");
+            System.Console.WriteLine("холост-1");
+            System.Console.WriteLine("семеянин-2");
+            System.Console.WriteLine("вразводе-3");
+            System.Console.WriteLine("Вдовец/вдова");
+           int Fimaly_Status  = int.Parse(Console.ReadLine());
+            if(Fimaly_Status==1)count++;
+            if(Fimaly_Status==2)count+=2;
+            if(Fimaly_Status==3)count++;
+            if(Fimaly_Status==4)count+=2;
+            System.Console.WriteLine("введите свой возраст:");
+            System.Console.WriteLine("До 25:-1");
+            System.Console.WriteLine("от 26-35:-2");
+            System.Console.WriteLine("от 36-62:-3");
+            System.Console.WriteLine("старше 63:-4");
+            int Age=int.Parse(Console.ReadLine());
+            if(Age==1)count+=0;
+            if(Age==2)count+=1;
+            if(Age==3)count+=2;
+            if(Age==4)count+=1;
+            System.Console.WriteLine("Гражданство:");
+            System.Console.WriteLine("Таджикистан-1");
+            System.Console.WriteLine("Другое-2");
+            int Citythenship=int.Parse(Console.ReadLine());
+            if(Citythenship==1)count+=1;
+            if(Citythenship==2)count+=0;
+            System.Console.WriteLine("сумма кредита от обшего дохода:");
+            System.Console.WriteLine("до 80%-1");
+            System.Console.WriteLine("80-150%-2");
+            System.Console.WriteLine("150-250%-3");
+            System.Console.WriteLine("Свыше 250%-4");
+
+            int cr=int.Parse(Console.ReadLine()); 
+            if(cr==1)count+=4;
+            if(cr==2)count+=3;
+            if(cr==3)count+=2;
+            if(cr==4)count+=1;
+            System.Console.WriteLine("История кредита:");
+            System.Console.WriteLine("более 3 закрытых историй-1");
+            System.Console.WriteLine("1-2 закрытых историй-2");
+            System.Console.WriteLine("нет кредитной истории-3");
+            int ch=int.Parse(Console.ReadLine());
+            if(ch==1)count+=2;
+            if(ch==2)count+=1;
+            if(ch==3)count--;
+            System.Console.WriteLine("просрочка в кредитной истории");
+            System.Console.WriteLine("свыше 7 раз-1");
+            System.Console.WriteLine(" 5-7 раз-2");
+            System.Console.WriteLine(" 4 раз-3");
+            System.Console.WriteLine("до 3 раз-4");
+            int Och=int.Parse(Console.ReadLine());
+             if(Och==1)count-=3;
+            if(Och==2)count-=2;
+            if(Och==3)count-=1;
+            if(Och==4)count+=0;
+            System.Console.WriteLine("Цель кредита");
+             System.Console.WriteLine("бытовая техника-1");
+              System.Console.WriteLine("ремонт-2");
+               System.Console.WriteLine("телефон-3");
+                System.Console.WriteLine("прочее-4");
+                int gc=int.Parse(Console.ReadLine());
+                 if(gc==1)count+=2;
+                 if(gc==2)count+=1;
+                 if(gc==3)count+=0;
+                 if(gc==4)count-=1;
+            System.Console.WriteLine("Срок кредита");
+            System.Console.WriteLine("более 12 мес-1");
+            System.Console.WriteLine("до 12 мес-2");
+            int dl=int.Parse(Console.ReadLine());
+                 if(dl==1)count+=1;
+                 if(dl==2)count+=1;
+
+            comm.CommandText=$"INSERT INTO U_App ([Sex],[Fimaly_Status],[Age],[Citythenship],[Loan amount of total income],[Credit_hitory],[Overdue credit history],[goal_credit],[deadLine])values({sex},{Fimaly_Status},{Fimaly_Status},{Age},{Citythenship},{cr},{ch},{Och},{gc},{dl})";
+            comm.ExecuteReader();
         }
 
     }
